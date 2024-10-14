9af6e44a11a6b3d49b8061ea7db4cb1e2421989b
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-10-14 16:09:23 -0400

    half way done

---
+++ app.js
@@ -0,0 +1,65 @@
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+const newArray = nums.map((currentElement)=>{
+    return currentElement * 2 ;
+
+});
+console.log(newArray);
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const [firstTopping, secondTopping] = pizzaToppings
+// console.log (firstTopping);
+// console.log (secondTopping);
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+//   const {make, model} = car
+//   console.log(make);
+//   console.log(model);
+
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+duplicateArray = [...pizzaToppings];
+console.log(duplicateArray);
+
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  const myCar = {...car};
+  myCar.model = 'q7'
+  console.log(car);
+  console.log(myCar);
+  // Change the `model` property of `myCar` to 'q7'. Log both objects.
+  
+  // Your code here
+  
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+const userProfile = {
+    username:'Jason',
+    age: 30
+}
+
+const propertyName = "username"
+
+userProfile[propertyName] = "Max"
+
+console.log(userProfile)
+
+
